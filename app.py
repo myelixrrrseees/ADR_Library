0f1714f88f11b56f11b2adb5682f4a6992ea3a05
myelixirrrseees <seees3d@gmail.com>
2022-11-08 07:43:22 +0700

    Добавьте файлы проекта.

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/home')
+def index():
+    return "Hello world"
+
+
+@app.route('/about')
+def about():
+    return "About page"
+
+
+@app.route('/user/<string:name>/<int:id>')
+def user(name, id):
+    return "User page" + name + " - " + id
+
+
+if __name__ == "__main__":
+    app.run(debug=False)
