7d5c36621e81442b2b4e3e0f6373020d49d4c555
myelixirrrseees <myelixirrr@yandex.ru>
2022-12-06 12:57:38 +0700

    con

--- models.py
+++ models.py
@@ -1,19 +1,28 @@
-from config import db
+from __init__ import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
 
 
-class Login(db.Model):
-    __tablename__ = 'auth'
+class Users(db.Model):
+    __tablename__ = 'Users'
 
     id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(50), unique=True)
+    name = db.Column(db.String(255), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
 
 
-class Book(db.Model):
-    __tablename__ = 'books'
+class Books(db.Model):
+    __tablename__ = 'Books'
+
     id = db.Column(db.Integer(), primary_key=True)
     title = db.Column(db.String(100), unique=True, nullable=False)
     author = db.Column(db.String(100), nullable=False)
     language = db.Column(db.String(50), nullable=False)
     year = db.Column(db.Integer())
     pages = db.Column(db.Integer())
+
+
+
+
+
--- routes.py
+++ routes.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
-from config import app, db
+from flask import Flask, render_template, request, redirect, url_for, flash
+
+from werkzeug.security import generate_password_hash, check_password_hash
+from __init__ import app, db
 
 
 @app.route('/')
@@ -11,7 +12,7 @@ def welcome():
 
 @app.route('/mainpage')
 def main_page():
-    return render_template("main_page.html")
+    return render_template("mainpage.html")
 
 
 @app.route('/library')
@@ -29,32 +30,36 @@ def news():
     return render_template("news.html")
 
 
-
 @app.route('/regestration', methods=['GET', 'POST'])
-def registr():
-    from models import Login
+def regestration():
+    from models import Users
 
     if request.method == "POST":
 
+        email = request.form['user_email']
         name = request.form['user_name']
         password = request.form['user_password']
         second_password = request.form['second_user_password']
 
-        if second_password == password:
-    
-            login = Login(name=name, password=password)
-        
-            try:
-                db.session.add(login)
-                db.session.commit()
-                return redirect('/')
-            except:
-                return "Нет"
-        else:
+        user = Users.query.filter_by(email=email)
+
+        if user :
+            flash('Email already exists')
             return redirect('/regestration')
-    else:
-        return render_template("regestration.html")
+
+        if second_password == password:
+
+            login = Users(email=email, name=name, password=generate_password_hash(password))
+
+            db.session.add(login)
+            db.session.commit()
+            return redirect('/mainpage')
+        else:
+            flash('Password is not correct. Try one more.')
+            return redirect('/regestration')
+
+    else: return render_template("regestration.html")
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8080)
